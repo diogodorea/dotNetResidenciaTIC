224354617df5644bb78547af9d0efe742f0d8f74
diogo dorea <diogodorea@gmail.com>
2023-11-09 17:06:37 -0300

    Testes de tipos de dados

--- Semana1/MeuPrimeiroProjeto/Program.cs
+++ Semana1/MeuPrimeiroProjeto/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+#region Teste de tipos de dados
+
+    int tipoInteiro;
+    double tipoDouble;
+    string tipoString;
+    long tipoLong;
+
+    tipoInteiro = int.MaxValue;
+    tipoLong = int.MaxValue;
+    tipoString = "100";
+    tipoInteiro=int.Parse(tipoString);
+    Console.WriteLine("O Máximo valor positivo intero é: "+ tipoInteiro);
+    Console.WriteLine("O Máximo valor positivo long é: "+ tipoLong);
+    
+
+#endregion
+
+#region Operadores
+
+
+#endregion
