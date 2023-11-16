e07ce10491e71686d19cf28e4b161b3a0239ceeb
Diogo Dorea <diogodorea@gmail.com>
2023-11-16 17:27:53 -0300

    Aula do dia 16-11 semana 2

--- Semana2/exemplosAula/Program.cs
+++ Semana2/exemplosAula/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region
+
+for(int i=1; i<+30; i++){
+    if(i%3 = 0 || i % 4 =0){
+        
+
+    }
+}
+
+#endregion
+
+#region 
+Console.WriteLine("Informe uma string: ");
+string? str = Console.ReadLine();
+Console.WriteLine($"A string foi {str}");
+string[] strSlitted? = str.Split();
+#endregion
+
+#region 
+
+#endregion
