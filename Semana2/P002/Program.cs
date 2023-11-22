172d55ab989b595ca8fced35bd57040b4df90864
Diogo Dorea <diogodorea@gmail.com>
2023-11-22 04:05:12 -0300

    gerenciador de tarefas

--- Semana2/P002/Program.cs
+++ Semana2/P002/Program.cs
@@ -14,3 +14,41 @@
         this.Vencimento = DateTime.Now;
     }
 }
+
+public class GerenciadorTarefas
+{
+    private List<Tarefa> listaTarefas;
+
+    public GerenciadorTarefas() {
+        listaTarefas = new List<Tarefa>();
+    }
+
+    public void AdicionarTarefa(Tarefa tarefa) {
+        listaTarefas.Add(tarefa);
+    }
+    
+    public void ListarTarefas() {
+        foreach (Tarefa tarefa in listaTarefas) {
+            Console.WriteLine($"ID: {tarefa.Id} - Título: {tarefa.Titulo} - Descrição: {tarefa.Descricao} - Concluída: {tarefa.Concluida}");
+        }
+    }
+
+    public void RemoverTarefa(int id)
+    {
+        Tarefa tarefa = listaTarefas.Find(t => t.Id == id);
+        if (tarefa != null)
+        {
+            listaTarefas.Remove(tarefa);
+        }
+    }
+
+    public void AtualizarTarefa(int id, Tarefa novaTarefa) {
+        Tarefa tarefa = listaTarefas.Find(t => t.Id == id);
+        if (tarefa != null)
+        {
+            tarefa.Titulo = novaTarefa.Titulo;
+            tarefa.Descricao = novaTarefa.Descricao;
+            tarefa.Concluida = novaTarefa.Concluida;
+        }
+    }
+}
