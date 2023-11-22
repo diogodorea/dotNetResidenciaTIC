031f853b87333a0f2c28d21137b1b88e451e1bb5
Diogo Dorea <diogodorea@gmail.com>
2023-11-22 03:57:37 -0300

    nova classe tarefa

--- Semana2/P002/Program.cs
+++ Semana2/P002/Program.cs
@@ -1,7 +1,16 @@
 ﻿public class Tarefa
 {
+    
+    private static int nextId = 1;
+    public DateTime dtCriacao = DateTime.Now;
     public int Id { get; set; }
     public string Titulo { get; set; }
     public string Descricao { get; set; }
+    public DateTime Vencimento { get; set; }
     public bool Concluida { get; set; }
+
+    public Tarefa() {
+        this.Id = nextId++;
+        this.Vencimento = DateTime.Now;
+    }
 }
