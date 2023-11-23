68f0ea2e28571da72269165694462dda1c562ed4
Diogo Dorea <diogodorea@gmail.com>
2023-11-23 17:49:18 -0300

    Semana 3

---
+++ Semana3/exemplosAula/Program.cs
@@ -0,0 +1,22 @@
+﻿#region 
+//exercicio 1
+(String, int) GetTupla (String nome, int idade) {
+    return (nome, idade);
+}
+Console.WriteLine($"Digite um nome e uma idade: ");
+var (nome, idade) = GetTupla(Console.ReadLine(), int.Parse(Console.ReadLine()));
+Console.WriteLine($"Nome: {nome}, Idade: {idade}");
+#endregion
+
+#region
+List<(String nome, int idade)> lista = new ();
+lista.Add(("João", 20));
+lista.Add(("Maria", 30));
+lista.Add(("José", 40));
+lista.Add(("Pedro", 50));
+
+var
+
+
+
+#endregion
