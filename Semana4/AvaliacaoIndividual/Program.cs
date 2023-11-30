b8e1b3f274429d3bdcf1d95bee963f2a71774f92
Diogo Dorea <diogodorea@gmail.com>
2023-11-30 16:18:43 -0300

    commit3

--- Semana4/AvaliacaoIndividual/Program.cs
+++ Semana4/AvaliacaoIndividual/Program.cs
@@ -1,39 +1,64 @@
-﻿public abstract class Pessoa {
+﻿public abstract class Pessoa
+{
+    private string _cpf;
+
     public string Nome { get; set; }
-    public string Cpf { get; set; }
+
+    public string Cpf
+    {
+        get { return _cpf; }
+        set
+        {
+            if (value != null && value.Length == 11 && value.Trim().Length == 11)
+            {
+                _cpf = value;
+            }
+            else
+            {
+                throw new ArgumentException("CPF deve conter 11 caracteres alfanuméricos.");
+            }
+        }
+    }
+
     public DateTime DataNascimento { get; set; }
-    public Pessoa() { }
-  
-}
 
-public class Advogado : Pessoa{
-
-    String CNA { get; set; }
-
-    public Advogado(string cNA)
+    public Pessoa(string nome, string cpf, DateTime dataNascimento)
     {
-        CNA = cNA;
+        Nome = nome;
+        Cpf = cpf;
+        DataNascimento = dataNascimento;
     }
 }
 
-public class Cliente : Pessoa{
 
-    String Ecivil { get; set; }
+public class Advogado : Pessoa {
+    public string CNA { get; set; }
 
-    public Cliente(string ecivil)
-    {
-        Ecivil = ecivil;
+    public Advogado(string nome, string cpf, DateTime dataNascimento) : base(nome, cpf, dataNascimento) {
+        
     }
+}
 
-    String Profissão { get; set; }
-
+public class Cliente : Pessoa {
+    public string profissao { get; set; }
+    public string Ecivil { get; set; }
+    public Cliente(string nome, string cpf, DateTime dataNascimento, string ecivil) : base(nome, cpf, dataNascimento) {
+    }
 }
 
 public class Program {
     public static void Main(string[] args) {
         List<Advogado> advogados = new List<Advogado>();
         List<Cliente> clientes = new List<Cliente>();
-        advogados.Add(new Advogado("123456789")) : base("João", "123456789", new DateTime(1990, 1, 1));
+        advogados.Add(new Advogado("João", "123456789", new DateTime(1990, 1, 1)));
+        advogados.Add(new Advogado("Maria", "987654321", new DateTime(1990, 1, 1)));
+        clientes.Add(new Cliente("José", "123456789", new DateTime(1990, 1, 1), "Casado"));
+        clientes.Add(new Cliente("Ana", "987654321", new DateTime(1990, 1, 1), "Solteiro"));
+        
+        foreach var in advogados {
+            Console.WriteLine(var.Nome);
+        }
+        
 
         
     }
