8dbd96da39fe01446345f66d63cc083d73f8e71a
Diogo Dorea <diogodorea@gmail.com>
2023-11-30 15:10:15 -0300

    atividade 1

--- Semana4/exerciciosAula/Program.cs
+++ Semana4/exerciciosAula/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Veiculo {
+    public string Cor { get; set; }
+    public string Modelo { get; set; }
+    public int Ano { get; set; }
+
+      
+    public Veiculo(string cor, string modelo, int ano) {
+        Cor = cor;
+        Modelo = modelo;
+        Ano = ano;
+    }
+
+    public void toString (){
+        Console.WriteLine("Cor: {0}", Cor);
+        Console.WriteLine("Modelo: {0}", Modelo);
+        Console.WriteLine("Ano: {0}", Ano);
+    
+    }
+}
+
+public class Program {
+    public static void Main(string[] args)
+    {
+        var carro = new Veiculo("Azul", "Fusca", 1970);
+        var moto = new Veiculo("Vermelha", "CG", 2010);
+        var caminhao = new Veiculo("Branco", "Mercedes", 2015);
+        
+        carro.toString();
+        moto.toString();
+        caminhao.toString();
+    }
+}
