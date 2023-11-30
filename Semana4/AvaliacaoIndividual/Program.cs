ff4833849984f81f02474deaa8e933eb7390c71d
Diogo Dorea <diogodorea@gmail.com>
2023-11-30 15:35:00 -0300

    avaliação individual base

---
+++ Semana4/AvaliacaoIndividual/Program.cs
@@ -0,0 +1,20 @@
+﻿public abstract class Pessoa {
+    public string Nome { get; set; }
+    public string Cpf { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public Pessoa(string nome, string cpf, string telefone, string endereco, string email, string senha, string tipo) {
+        Nome = nome;
+        Cpf = cpf;
+        Telefone = telefone;
+        Endereco = endereco;
+        Email = email;
+        Senha = senha;
+        Tipo = tipo;
+    }
+    public Pessoa() { }
+    public abstract void Cadastrar();
+    public abstract void Listar();
+    public abstract void Buscar(string busca);
+    public abstract void Remover(string busca);
+    public abstract void Editar(string busca);
+}
